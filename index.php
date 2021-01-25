08e7067fb10e32194347f3593c85b889461cb93d
Priyanka Singh <71539429+priyankars669@users.noreply.github.com>
2021-01-25 14:37:41 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>Priyanka Singh 6420c462</title></head>
+<body style="font-family:Helvetica">
+	<h1>Welcome to Rock-Paper-Scissors Game</h1>
+	<a href="login.php">Please Log In</a>
+	<p> Attempt to go to <a href="game.php">Game</a> without logging in -it will fail with with an error message.</p>
+	</body>
+	</html>
---
+++ login.php
@@ -0,0 +1,54 @@
+<?php 
+if ( isset($_POST['cancel'] ) ) {
+    header("Location: index.php");
+    return;
+}
+$salt = 'XyZzy12*_';
+$stored_hash = '1a52e17fa899cf40fb04cfc42e6352f1'; 
+$failure = false;
+if ( isset($_POST['who']) && isset($_POST['pass']) ) {
+    if ( strlen($_POST['who']) < 1 || strlen($_POST['pass']) < 1 ) {
+        $failure = "User name and password are required";
+    } else {
+        $check = hash('md5', $salt . $_POST['pass']);
+        if ( $check == $stored_hash ) {
+       
+            header("Location: game.php?name=".urlencode($_POST['who']));
+            return;
+        } else {
+            $failure = "Incorrect password";
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Priyanka Singh 6420c462</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Please Log In</h1>
+		<?php
+			
+			if ( $failure !== false ) {
+			  
+			    echo('<p style="color: red;">'.htmlentities($failure)."</p>\n");
+			}
+		?>
+		<form method="POST">
+			<label for="who">User Name</label>
+			<input type="text" name="who" id="who"><br/>
+			<label for="pass">Password</label>
+			<input type="text" name="pass" id="pass"><br/>
+			<input type="submit" value="Log In">
+			<input type="submit" name="cancel" value="Cancel">
+		</form>
+		<p>
+			For a password hint, view source and find a password hint
+			in the HTML comments.
+			<!-- password is....php123-->
+		</p>
+	</div>
+</body>
